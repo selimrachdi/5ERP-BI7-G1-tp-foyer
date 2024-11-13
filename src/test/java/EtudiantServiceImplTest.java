0323b616768b4e91a344d437213ab24141da0cc5
HendiliMohamedAziz <83289023+HendiliMohamedAziz@users.noreply.github.com>
2024-11-13 13:45:40 +0100

    remove profile setup

--- src/test/java/EtudiantServiceImplTest.java
+++ src/test/java/EtudiantServiceImplTest.java
@@ -19,7 +19,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-@ActiveProfiles("test")
 class EtudiantServiceImplTest {
 
     @InjectMocks
